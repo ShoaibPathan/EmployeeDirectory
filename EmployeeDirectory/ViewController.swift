4160eaa646d3369057e020d717c7927d0cc7471b
Patrick Maltagliati <patmalt@gmail.com>
2020-09-08 13:13:26 -0500

    Initial Commit

---
+++ EmployeeDirectory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory
+//
+//  Created by Patrick Maltagliati on 9/8/20.
+//  Copyright © 2020 Patrick Maltagliati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
